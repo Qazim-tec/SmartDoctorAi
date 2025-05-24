b8a95b12cfdca31ef7004dbb261e4cd268675c4a
DAUDU QAZIM <yetkemsupper@gmail.com>
2025-05-24 18:50:25 +0100

    feat-Initialpush

--- HealthTech/obj/Debug/net8.0/HealthTech.AssemblyInfo.cs
+++ HealthTech/obj/Debug/net8.0/HealthTech.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthTech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c934e1f7c4f0ec89bed66afae7fef994798973d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthTech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthTech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
